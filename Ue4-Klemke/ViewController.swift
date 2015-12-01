95f47a175af574d467594330d10eb14aa2039753
Julian Fiebig <julianfiebig@googlemail.com>
2015-12-01 15:23:22 +0100

    initial commit

--- Ue4-Klemke/ViewController.swift
+++ Ue4-Klemke/ViewController.swift
@@ -145,7 +145,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
-    
-}
+    @IBAction func back(segue : UIStoryboardSegue){
+        
+    }
 
+}
